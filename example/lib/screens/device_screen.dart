68f913aa92230e2cef805d2ad5a513a546ccd4d4
HHD <mcgegan@gmail.com>
2025-01-22 20:39:49 +0900

    flutter_blue_plus_template begin

---
+++ example/lib/screens/device_screen.dart
@@ -0,0 +1,346 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+import '../widgets/service_tile.dart';
+import '../widgets/characteristic_tile.dart';
+import '../widgets/descriptor_tile.dart';
+import '../utils/snackbar.dart';
+import '../utils/extra.dart';
+
+//added by hhd begin
+import '../userWidget/evse_pilot_status_tile.dart';
+import '../userWidget/blank_tile.dart';
+//added by hhd end
+
+class DeviceScreen extends StatefulWidget {
+  final BluetoothDevice device;
+
+  const DeviceScreen({Key? key, required this.device}) : super(key: key);
+
+  @override
+  State<DeviceScreen> createState() => _DeviceScreenState();
+}
+
+class _DeviceScreenState extends State<DeviceScreen> {
+  int? _rssi;
+  int? _mtuSize;
+  BluetoothConnectionState _connectionState = BluetoothConnectionState.disconnected;
+  List<BluetoothService> _services = [];
+  bool _isDiscoveringServices = false;
+  bool _isConnecting = false;
+  bool _isDisconnecting = false;
+  //added by hhd begin
+  List<BluetoothDescriptor> _descriptors = [];
+  List<BluetoothCharacteristic> _characteristics = [];
+  //late EvsePilotStatusTile evsePilotStatusTile;// = EvsePilotStatusTile();
+  Widget userWidget = BlankTile();
+  
+  BluetoothService? pilotService;
+  Timer? pilotReadTimer;
+  //added by hhd end
+
+  late StreamSubscription<BluetoothConnectionState> _connectionStateSubscription;
+  late StreamSubscription<bool> _isConnectingSubscription;
+  late StreamSubscription<bool> _isDisconnectingSubscription;
+  late StreamSubscription<int> _mtuSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _connectionStateSubscription = widget.device.connectionState.listen((state) async {
+      _connectionState = state;
+      if (state == BluetoothConnectionState.connected) {
+        _services = []; // must rediscover services
+      }
+      if (state == BluetoothConnectionState.connected && _rssi == null) {
+        _rssi = await widget.device.readRssi();
+      }
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    _mtuSubscription = widget.device.mtu.listen((value) {
+      _mtuSize = value;
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    _isConnectingSubscription = widget.device.isConnecting.listen((value) {
+      _isConnecting = value;
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    _isDisconnectingSubscription = widget.device.isDisconnecting.listen((value) {
+      _isDisconnecting = value;
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    // added by hhd begin
+    Timer.periodic(Duration(milliseconds: 2000), (timer) async {
+      debugPrint('mounted=$mounted');
+      debugPrint('isConnected=$isConnected');
+      if (mounted && isConnected) {
+        onDiscoverServicesPressed();
+      }
+      if (mounted && isConnected && (_services.length > 0)) {
+        debugPrint(_services.toString());
+
+        for (BluetoothService s in _services) {
+          debugPrint('HHD: service: ${s.uuid}');
+          if (s.uuid.toString() == pilot_service_uuid) {
+            pilotService = s;
+            try {
+              _characteristics = await pilotService!.characteristics;
+            } catch (e) {
+              debugPrint('HHD: error: ${e.toString()}');
+            }
+          }
+          for (BluetoothCharacteristic c in s.characteristics) {
+            debugPrint('HHD: characteristic: ${c.uuid}');
+            for (BluetoothDescriptor d in c.descriptors) {
+              debugPrint('HHD: descriptor: ${d.uuid}');
+            }
+          }
+        }
+        if (pilotService != null) {
+          userWidget = buildPilotStatusTile(context, pilotService);
+        }
+        timer.cancel();
+        
+        return;
+      }
+    });
+
+    // added by hhd end
+  }
+
+  @override
+  void dispose() {
+    _connectionStateSubscription.cancel();
+    _mtuSubscription.cancel();
+    _isConnectingSubscription.cancel();
+    _isDisconnectingSubscription.cancel();
+    super.dispose();
+  }
+
+  //added by hhd begin
+
+  Widget buildPilotStatusTile(BuildContext context, BluetoothService? service) {
+    if (service == null) {
+      return Container();
+    }
+    return EvsePilotStatusTile(service: service);
+  }
+  //added by hhd end
+
+  bool get isConnected {
+    return _connectionState == BluetoothConnectionState.connected;
+  }
+
+  Future onConnectPressed() async {
+    try {
+      await widget.device.connectAndUpdateStream();
+      Snackbar.show(ABC.c, "Connect: Success", success: true);
+    } catch (e) {
+      if (e is FlutterBluePlusException && e.code == FbpErrorCode.connectionCanceled.index) {
+        // ignore connections canceled by the user
+      } else {
+        Snackbar.show(ABC.c, prettyException("Connect Error:", e), success: false);
+        print(e);
+      }
+    }
+  }
+
+  Future onCancelPressed() async {
+    try {
+      await widget.device.disconnectAndUpdateStream(queue: false);
+      Snackbar.show(ABC.c, "Cancel: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Cancel Error:", e), success: false);
+      print(e);
+    }
+  }
+
+  Future onDisconnectPressed() async {
+    try {
+      await widget.device.disconnectAndUpdateStream();
+      Snackbar.show(ABC.c, "Disconnect: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Disconnect Error:", e), success: false);
+      print(e);
+    }
+  }
+
+  Future onDiscoverServicesPressed() async {
+    if (mounted) {
+      setState(() {
+        _isDiscoveringServices = true;
+      });
+    }
+    try {
+      _services = await widget.device.discoverServices();
+      Snackbar.show(ABC.c, "Discover Services: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Discover Services Error:", e), success: false);
+      print(e);
+    }
+    if (mounted) {
+      setState(() {
+        _isDiscoveringServices = false;
+      });
+    }
+  }
+
+  Future onRequestMtuPressed() async {
+    try {
+      await widget.device.requestMtu(223, predelay: 0);
+      Snackbar.show(ABC.c, "Request Mtu: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Change Mtu Error:", e), success: false);
+      print(e);
+    }
+  }
+
+  List<Widget> _buildServiceTiles(BuildContext context, BluetoothDevice d) {
+    return _services
+        .map(
+          (s) => ServiceTile(
+            service: s,
+            characteristicTiles: s.characteristics.map((c) => _buildCharacteristicTile(c)).toList(),
+          ),
+        )
+        .toList();
+  }
+
+  CharacteristicTile _buildCharacteristicTile(BluetoothCharacteristic c) {
+    return CharacteristicTile(
+      characteristic: c,
+      descriptorTiles: c.descriptors.map((d) => DescriptorTile(descriptor: d)).toList(),
+    );
+  }
+
+  Widget buildSpinner(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.black12,
+          color: Colors.black26,
+        ),
+      ),
+    );
+  }
+
+  Widget buildRemoteId(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text('${widget.device.remoteId}'),
+    );
+  }
+
+  Widget buildRssiTile(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        isConnected ? const Icon(Icons.bluetooth_connected) : const Icon(Icons.bluetooth_disabled),
+        Text(((isConnected && _rssi != null) ? '${_rssi!} dBm' : ''), style: Theme.of(context).textTheme.bodySmall)
+      ],
+    );
+  }
+
+  Widget buildGetServices(BuildContext context) {
+    return IndexedStack(
+      index: (_isDiscoveringServices) ? 1 : 0,
+      children: <Widget>[
+        TextButton(
+          child: const Text("Get Services"),
+          onPressed: onDiscoverServicesPressed,
+        ),
+        const IconButton(
+          icon: SizedBox(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Colors.grey),
+            ),
+            width: 18.0,
+            height: 18.0,
+          ),
+          onPressed: null,
+        )
+      ],
+    );
+  }
+
+  Widget buildMtuTile(BuildContext context) {
+    return ListTile(
+        title: const Text('MTU Size'),
+        subtitle: Text('$_mtuSize bytes'),
+        trailing: IconButton(
+          icon: const Icon(Icons.edit),
+          onPressed: onRequestMtuPressed,
+        ));
+  }
+
+  Widget buildConnectButton(BuildContext context) {
+    return Row(children: [
+      if (_isConnecting || _isDisconnecting) buildSpinner(context),
+      TextButton(
+          onPressed: _isConnecting ? onCancelPressed : (isConnected ? onDisconnectPressed : onConnectPressed),
+          child: Text(
+            _isConnecting ? "CANCEL" : (isConnected ? "DISCONNECT" : "CONNECT"),
+            style: Theme.of(context).primaryTextTheme.labelLarge?.copyWith(color: Colors.white),
+          ))
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger(
+      key: Snackbar.snackBarKeyC,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.device.platformName),
+          actions: [buildConnectButton(context)],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              buildRemoteId(context),
+              ListTile(
+                leading: buildRssiTile(context),
+                title: Text('Device is ${_connectionState.toString().split('.')[1]}.'),
+                trailing: buildGetServices(context),
+              ),
+              Visibility(
+                visible: false,
+                child: buildMtuTile(context),
+              ),
+              Visibility(
+                visible: false,
+                child: Column(
+                  children: _buildServiceTiles(context, widget.device),
+                ),
+              ),
+              const Divider(
+                height: 1.0,
+                thickness: 1.0,
+                color: Colors.grey,
+              ),
+              userWidget,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ example/lib/userWidget/evse_pilot_status_tile.dart
@@ -0,0 +1,296 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_blue_plus_example/widgets/descriptor_tile.dart';
+
+import "../utils/snackbar.dart";
+
+import 'evse_pilot_status_tile.dart';
+
+import 'dart:convert';
+
+final pilot_service_uuid = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+final cp_duty_uuid = "bcaf4dee-e9b1-4f61-8bfa-340554dc63fa";
+final cp_voltage_uuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+final cp_status_uuid = "ee17c408-d77e-4a12-9357-fd4e26206d8f";
+final pp_voltage_uuid = "6e99e63f-55a5-4a1e-9788-ed2c44101750";
+final pp_status_uuid = "1f3ef3d7-18be-44e5-82f7-ff8d498f4e4b";
+
+class EvsePilotStatusTile extends StatefulWidget {
+  final BluetoothService service;
+
+  const EvsePilotStatusTile({Key? key, required this.service}) : super(key: key);
+  @override
+  _EvsePilotStatusTileState createState() => _EvsePilotStatusTileState();
+}
+
+class _EvsePilotStatusTileState extends State<EvsePilotStatusTile> {
+  late BluetoothCharacteristic cpDutyCharacteristic;
+  late BluetoothCharacteristic cpVoltageCharacteristic;
+  late BluetoothCharacteristic cpStatusCharacteristic;
+  late BluetoothCharacteristic ppVoltageCharacteristic;
+  late BluetoothCharacteristic ppStatusCharacteristic;
+
+  String cpDutySetValue = 'Enter 0~100';
+
+  String _cpDuty = 'TBD';
+
+  String get cpDuty => _cpDuty;
+
+  set cpDuty(String value) {
+    _cpDuty = value;
+  }
+  String _cpVoltage = 'TBD';
+
+  String get cpVoltage => _cpVoltage;
+
+  set cpVoltage(String value) {
+    _cpVoltage = value;
+  }
+  String _cpStatus = 'TBD';
+
+  String get cpStatus => _cpStatus;
+
+  set cpStatus(String value) {
+    _cpStatus = value;
+  }
+  String _ppVoltage = 'TBD';
+
+  String get ppVoltage => _ppVoltage;
+
+  set ppVoltage(String value) {
+    _ppVoltage = value;
+  }
+  String ppStatus = 'TBD';
+
+  Future sendCpDuty() async {
+    if (cpDutySetValue.isEmpty) {
+      return;
+    }
+    try {
+      await cpDutyCharacteristic.write(utf8.encode(cpDutySetValue));
+      Snackbar.show(ABC.c, "Write: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Write Error:", e), success: false);
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Add your initialization code here
+    for (BluetoothCharacteristic c in widget.service.characteristics) {
+      if (c.uuid.toString() == cp_duty_uuid) {
+        cpDutyCharacteristic = c;
+        c.read().then((value) {
+          setState(() {
+            cpDuty = value.toString();
+          });
+        });
+      }
+      if (c.uuid.toString() == cp_voltage_uuid) {
+        cpVoltageCharacteristic = c;
+        c.read().then((value) {
+          setState(() {
+            cpVoltage = value.toString();
+          });
+        });
+      }
+      if (c.uuid.toString() == cp_status_uuid) {
+        cpStatusCharacteristic = c;
+        c.read().then((value) {
+          setState(() {
+            cpStatus = value.toString();
+          });
+        });
+      }
+      if (c.uuid.toString() == pp_voltage_uuid) {
+        ppVoltageCharacteristic = c;
+        c.read().then((value) {
+          setState(() {
+            ppVoltage = value.toString();
+          });
+        });
+      }
+      if (c.uuid.toString() == pp_status_uuid) {
+        ppStatusCharacteristic = c;
+        c.read().then((value) {
+          setState(() {
+            ppStatus = value.toString();
+          });
+        });
+      }
+    }
+    Timer.periodic(Duration(milliseconds: 1000), (timer) {
+      for (BluetoothCharacteristic c in widget.service.characteristics) {
+        if (c.uuid.toString() == cp_duty_uuid) {
+          c.read().then((value) {
+            setState(() {
+              cpDuty = String.fromCharCodes(value);
+              //cpDuty = value.toString();
+            });
+          });
+        }
+        if (c.uuid.toString() == cp_voltage_uuid) {
+          c.read().then((value) {
+            setState(() {
+              cpVoltage = String.fromCharCodes(value);
+              //cpVoltage = value.toString();
+            });
+          });
+        }
+        if (c.uuid.toString() == cp_status_uuid) {
+          c.read().then((value) {
+            setState(() {
+              cpStatus = String.fromCharCodes(value);
+              //cpStatus = value.toString();
+            });
+          });
+        }
+        if (c.uuid.toString() == pp_voltage_uuid) {
+          c.read().then((value) {
+            setState(() {
+              ppVoltage = String.fromCharCodes(value);
+              //ppVoltage = value.toString();
+            });
+          });
+        }
+        if (c.uuid.toString() == pp_status_uuid) {
+          c.read().then((value) {
+            setState(() {
+              ppStatus = String.fromCharCodes(value);
+              //ppStatus = value.toString();
+            });
+          });
+        }
+      }
+      setState(() {
+        debugPrint('evse_pilot_status_tile');
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(height: 10),
+          Text(
+            'EVSE Pilot Status',
+            textAlign: TextAlign.left,
+            style: Theme.of(context).textTheme.titleLarge,
+            ),
+          SizedBox(height: 10),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                Text(
+                'Set CP duty:',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              Container(
+                width: 200.0, // Set the desired width here
+                child: TextField(
+                  textAlign: TextAlign.right,
+                  decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: cpDutySetValue,
+                  labelStyle: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (value) {
+                  setState(() {
+                    cpDutySetValue = value;
+                  });
+                  },
+                ),
+                height: 40.0, // Set the desired height here
+              ),
+              IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () {
+                  // Add your onPressed code here
+                  sendCpDuty();
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                Text(
+                'CP Duty:',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                '${cpDuty} V',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                Text(
+                'CP Voltage:',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                '${cpVoltage} V',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                Text(
+                'CP Status:',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                '${cpStatus}',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                Text(
+                'PP Voltage:',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                '${ppVoltage} V',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                Text(
+                'PP Status:',
+                style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                '${ppStatus}',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
