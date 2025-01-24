b8507be316d589865bf3979c0afd5aaf421d7c76
HHD <mcgegan@gmail.com>
2025-01-24 14:43:52 +0900

    macos first build

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\user\ff_flutter_sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\user\developer\flutter_blue_plus_template\example"
+export "FLUTTER_ROOT=/Users/user/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user/workspace/flutter_blue_plus_template/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/user/workspace/flutter_blue_plus_template/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/user/workspace/flutter_blue_plus_template/example/.dart_tool/package_config.json"
